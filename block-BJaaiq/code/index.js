103e418f1e82417202721b17db5250e113b9c928
Prabhat <prabhatdbg21@gmail.com>
2022-12-19 14:56:46 +0530

    block-BJaaiq

--- block-BJaaiq/code/index.js
+++ block-BJaaiq/code/index.js
@@ -1,3 +1,4 @@
+/*
 let matrix = document.querySelector(`#matrix`)
 
 alert(`The content of the DOM is loaded`)
@@ -20,3 +21,38 @@ arrowTop.onclick = function() {
 window.addEventListener('scroll', function() {
     arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
 });
+*/
+
+let root = document.querySelector('ul');
+
+let max = 3;
+let index = 0;
+
+function addQuotes() {
+    for (let i = 0; i < max; i++) {
+        let li = document.createElement('li');
+        let blockquote = document.createElement('blockquote');
+        let cite = document.createElement('cite');
+        blockquote.innerText = quotes[index].quoteText;
+        cite.innerText = quotes[index].quoteAuthor;
+        li.append(blockquote, cite);
+
+        root.append(li);
+        index++;
+    }
+}
+
+
+document.addEventListener("scroll", () => {
+    let scrollTop = document.documentElement.scrollTop;
+    let clientHeight = document.documentElement.clientHeight;
+    let scrollHeight = document.documentElement.scrollHeight;
+    if (scrollTop + clientHeight >= scrollHeight  && index < quotes.length) {
+        addQuotes();
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    alert(`The content of the DOM is loaded`);
+    addQuotes() ;
+})
