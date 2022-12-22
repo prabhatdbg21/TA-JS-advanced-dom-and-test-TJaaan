fced01c02c6c3eb0391107744cd2d6a9e1990b06
Prabhat <prabhatdbg21@gmail.com>
2022-12-22 23:43:37 +0530

    block-BJaait

--- block-BJaait/code/index.js
+++ block-BJaait/code/index.js
@@ -1,30 +1,3 @@
-/*
-let ul = document.querySelector('ul');
-let input = document.querySelector('input');
-
-let listOfLi = [] ;
-
-function handleClick() {
-    if (input.value !== '') {
-        listOfLi.push(input.value);
-        createUI();
-    }
-}
-
-input.addEventListener('click', handleClick)
-
-function createUI (list = listOfLi , root = ul){
-    root.innerHTML = "" ;
-    let fragment = new DocumentFragment();
-    list.forEach((listItem) => {
-        let li = document.createElement('li')
-        li.innerText = listItem;
-        fragment.appendChild(li)
-    })
-    root.append(fragment)
-}
-*/
-
 var btn = document.querySelector('.add');
 var remove = document.querySelector('.draggable');
 
