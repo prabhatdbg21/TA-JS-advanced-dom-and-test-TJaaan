f47e73618d97e5cc0d3719c867b91ddc7d2a53d5
Prabhat <prabhatdbg21@gmail.com>
2023-01-09 21:44:47 +0530

    block-BJaajc

--- block-BJaajc/code/index.js
+++ block-BJaajc/code/index.js
@@ -1,37 +1,38 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-undef */
 function getFullName(firstName, lastName) {
-    fullName = `${firstName} ${lastName}`;
-    return fullName;
+  fullName = `${firstName} ${lastName}`;
+  return fullName;
 }
 
 function isPalindrome(value) {
-    let num = value.toString();
-    // find the length of a value
-    const len = num.length;
+  const num = value.toString();
+  // find the length of a value
+  const len = num.length;
 
-    // loop through half of the value
-    for (let i = 0; i < len / 2; i++) {
-    
-        // check if first and last string are same
-        if (num[i] !== num[len - 1 - i]) {
-            return false;
-        }
+  // loop through half of the value
+  for (let i = 0; i < len / 2; i++) {
+    // check if first and last string are same
+    if (num[i] !== num[len - 1 - i]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 function getCircumFrence(radius) {
-    const pi = 3.14
-    return `The circumference is ${2 * pi * radius}`
+  const pi = 3.14;
+  return `The circumference is ${2 * pi * radius}`;
 }
 
 function getArea(radius) {
-    const pi = 3.14
-    return `The getArea is ${pi * radius * radius}`
+  const pi = 3.14;
+  return `The getArea is ${pi * radius * radius}`;
 }
 
 module.exports = {
-    getFullName,
-    isPalindrome,
-    getCircumFrence,
-    getArea,
+  getFullName,
+  isPalindrome,
+  getCircumFrence,
+  getArea,
 };
